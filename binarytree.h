b455b6911d3932b7cb1a99757455c035b38afa1b
Ashwin Krishnamurthy <krishn_a1@denison.edu>
2024-03-27 11:57:48 -0400

    adding needed methods

--- binarytree.h
+++ binarytree.h
@@ -4,3 +4,23 @@
 // Name: Daniel Chu, Ashwin Krishnamurthy, Jianheng Guo
 // March 2024
 //======================================================
+
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+template <typename T>
+class BinaryTree {
+public:
+	BinaryTree(void);
+	~BinaryTree(void);
+	void insert(const T& item);
+	void remove (const T& item);
+	void search (const T& item) const;
+	void inOrderTreeWalk (const T& item);
+
+private:
+	struct Node {
+		T item;
+		Node* left;
+		Node* right;
+	};
